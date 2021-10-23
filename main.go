3dd59c4f78a3a811a88d34a1f3de3bc4da966993
Preetham <preetham.s@yellowmessenger.com>
2021-10-23 15:18:35 +0530

    multiple packages

--- main.go
+++ main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
+	// var card string = "Ace of Spades"
+
+	// card := "Ace of Spades"
+	// card = "Five of Diamonds"
+
+	card := newCard()
 	fmt.Println(card)
 }
+
+// Files in the same package can freely call functions defined in other files.
+// go run main.go newcard.go
---
+++ newcard.go
@@ -0,0 +1,5 @@
+package main
+
+func newCard() string {
+	return "Ace of Diamonds"
+}
